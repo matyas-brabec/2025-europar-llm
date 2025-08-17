f3c00b07cde4912301a0dd6c8af5a5756b9e6b74
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2025-08-17 22:11:29 +0200

    setup plots

--- measured-times/generate_all_graphs.sh
+++ measured-times/generate_all_graphs.sh
@@ -23,6 +23,14 @@ args_per_graph=(
     "gol@single-graph-style@no-paper-print@auto-boundaries ../gol-ampere.csv ../gol-ampere.reference.csv"
     "gol@single-graph-style@no-paper-print@auto-boundaries ../gol-volta.csv ../gol-volta.reference.csv"
 
+    "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-32-hopper.csv ../knn-32-hopper.reference.csv"
+    "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-32-ampere.csv ../knn-32-ampere.reference.csv"
+    "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-32-volta.csv ../knn-32-volta.reference.csv"
+
+    "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-1024-hopper.csv ../knn-1024-hopper.reference.csv"
+    "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-1024-ampere.csv ../knn-1024-ampere.reference.csv"
+    "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-1024-volta.csv ../knn-1024-volta.reference.csv"
+
     # logaritmic scale
     "hist@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../histogram-hexdump-ampere.csv ../histogram-hexdump-ampere.reference.csv log-scale_"
     "hist@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../histogram-hexdump-volta.csv ../histogram-hexdump-volta.reference.csv log-scale_"
@@ -36,29 +44,42 @@ args_per_graph=(
     "gol@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../gol-ampere.csv ../gol-ampere.reference.csv log-scale_"
     "gol@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../gol-volta.csv ../gol-volta.reference.csv log-scale_"
 
+    "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-32-hopper.csv ../knn-32-hopper.reference.csv log-scale_"
+    "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-32-ampere.csv ../knn-32-ampere.reference.csv log-scale_"
+    "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-32-volta.csv ../knn-32-volta.reference.csv log-scale_"
+
+    "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-1024-hopper.csv ../knn-1024-hopper.reference.csv log-scale_"
+    "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-1024-ampere.csv ../knn-1024-ampere.reference.csv log-scale_"
+    "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-1024-volta.csv ../knn-1024-volta.reference.csv log-scale_"
+
     # Graphs for the paper
 
     "hist@two-graphs-in-row-style@first-graph@with-log-scale ../histogram-loremipsum-hopper.csv ../histogram-loremipsum-hopper.reference.csv paper_"
     "hist@two-graphs-in-row-style@second-graph@with-log-scale ../histogram-hexdump-hopper.csv ../histogram-hexdump-hopper.reference.csv paper_"
     "gol@single-graph-style@with-log-scale ../gol-hopper.csv ../gol-hopper.reference.csv paper_"
+    "knn@two-graphs-in-row-style@first-graph@with-log-scale ../knn-32-hopper.csv ../knn-32-hopper.reference.csv paper_"
+    "knn@two-graphs-in-row-style@second-graph@with-log-scale ../knn-32-ampere.csv ../knn-32-ampere.reference.csv paper_"
 
     "hist@two-graphs-in-row-style@first-graph@with-log-scale@narrow-hist ../histogram-loremipsum-hopper.csv ../histogram-loremipsum-hopper.reference.csv paper_narrow_"
     "hist@two-graphs-in-row-style@second-graph@with-log-scale@narrow-hist ../histogram-hexdump-hopper.csv ../histogram-hexdump-hopper.reference.csv paper_narrow_"
     "gol@single-graph-style@with-log-scale@narrow-gol ../gol-hopper.csv ../gol-hopper.reference.csv paper_narrow_"
+    "knn@two-graphs-in-row-style@first-graph@with-log-scale@narrow-knn ../knn-32-hopper.csv ../knn-32-hopper.reference.csv paper_narrow_"
+    "knn@two-graphs-in-row-style@second-graph@with-log-scale@narrow-knn ../knn-32-ampere.csv ../knn-32-ampere.reference.csv paper_narrow_"
 )
 
 echo "Printing graphs"
 echo
 
-(
-    echo "Setting up Python environment"
-    cd "$(dirname "$printer_path")" || exit 1
+echo "Setting up Python environment"
 
-    python3 -m venv .venv
-    source .venv/bin/activate
-    python3 -m pip install --upgrade pip
-    python3 -m pip install -r requirements.txt
-)
+pushd "$(dirname "$printer_path")" || exit 1
+
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+popd || exit 1
 
 for args in "${args_per_graph[@]}"; do
     echo "python3 $printer_path $args"
