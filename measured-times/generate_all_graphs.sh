55ffe5820edf445b302d542b0743b27c67496442
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2025-11-16 01:11:29 +0100

    update graphs

--- measured-times/generate_all_graphs.sh
+++ measured-times/generate_all_graphs.sh
@@ -3,6 +3,7 @@
 script_dir=$(dirname "$0")
 output_dir=$script_dir/graphs
 
+mkdir -p "$output_dir"
 cd "$output_dir" || exit 1
 
 printer_path="$script_dir/../../framework/print-graph.py"
@@ -14,57 +15,86 @@ args_per_graph=(
     "hist@single-graph-style@no-paper-print@auto-boundaries ../histogram-hexdump-ampere.csv ../histogram-hexdump-ampere.reference.csv"
     "hist@single-graph-style@no-paper-print@auto-boundaries ../histogram-hexdump-volta.csv ../histogram-hexdump-volta.reference.csv"
     "hist@single-graph-style@no-paper-print@auto-boundaries ../histogram-hexdump-hopper.csv ../histogram-hexdump-hopper.reference.csv"
+    "hist@single-graph-style@no-paper-print@auto-boundaries ../histogram-hexdump-blackwell.csv ../histogram-hexdump-blackwell.reference.csv"
 
     "hist@single-graph-style@no-paper-print@auto-boundaries ../histogram-loremipsum-ampere.csv ../histogram-loremipsum-ampere.reference.csv"
     "hist@single-graph-style@no-paper-print@auto-boundaries ../histogram-loremipsum-volta.csv ../histogram-loremipsum-volta.reference.csv"
     "hist@single-graph-style@no-paper-print@auto-boundaries ../histogram-loremipsum-hopper.csv ../histogram-loremipsum-hopper.reference.csv"
+    "hist@single-graph-style@no-paper-print@auto-boundaries ../histogram-loremipsum-blackwell.csv ../histogram-loremipsum-blackwell.reference.csv"
 
     "gol@single-graph-style@no-paper-print@auto-boundaries ../gol-hopper.csv ../gol-hopper.reference.csv"
     "gol@single-graph-style@no-paper-print@auto-boundaries ../gol-ampere.csv ../gol-ampere.reference.csv"
     "gol@single-graph-style@no-paper-print@auto-boundaries ../gol-volta.csv ../gol-volta.reference.csv"
+    "gol@single-graph-style@no-paper-print@auto-boundaries ../gol-blackwell.csv ../gol-blackwell.reference.csv"
 
     "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-32-hopper.csv ../knn-32-hopper.reference.csv"
     "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-32-ampere.csv ../knn-32-ampere.reference.csv"
     "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-32-volta.csv ../knn-32-volta.reference.csv"
+    "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-32-blackwell.csv ../knn-32-blackwell.reference.csv"
 
     "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-1024-hopper.csv ../knn-1024-hopper.reference.csv"
     "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-1024-ampere.csv ../knn-1024-ampere.reference.csv"
     "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-1024-volta.csv ../knn-1024-volta.reference.csv"
+    "knn@single-graph-style@no-paper-print@auto-boundaries ../knn-1024-blackwell.csv ../knn-1024-blackwell.reference.csv"
 
     # logaritmic scale
     "hist@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../histogram-hexdump-ampere.csv ../histogram-hexdump-ampere.reference.csv log-scale_"
     "hist@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../histogram-hexdump-volta.csv ../histogram-hexdump-volta.reference.csv log-scale_"
     "hist@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../histogram-hexdump-hopper.csv ../histogram-hexdump-hopper.reference.csv log-scale_"
+    "hist@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../histogram-hexdump-blackwell.csv ../histogram-hexdump-blackwell.reference.csv log-scale_"
 
     "hist@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../histogram-loremipsum-ampere.csv ../histogram-loremipsum-ampere.reference.csv log-scale_"
     "hist@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../histogram-loremipsum-volta.csv ../histogram-loremipsum-volta.reference.csv log-scale_"
     "hist@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../histogram-loremipsum-hopper.csv ../histogram-loremipsum-hopper.reference.csv log-scale_"
+    "hist@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../histogram-loremipsum-blackwell.csv ../histogram-loremipsum-blackwell.reference.csv log-scale_"
 
     "gol@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../gol-hopper.csv ../gol-hopper.reference.csv log-scale_"
     "gol@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../gol-ampere.csv ../gol-ampere.reference.csv log-scale_"
     "gol@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../gol-volta.csv ../gol-volta.reference.csv log-scale_"
+    "gol@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../gol-blackwell.csv ../gol-blackwell.reference.csv log-scale_"
 
     "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-32-hopper.csv ../knn-32-hopper.reference.csv log-scale_"
     "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-32-ampere.csv ../knn-32-ampere.reference.csv log-scale_"
     "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-32-volta.csv ../knn-32-volta.reference.csv log-scale_"
+    "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-32-blackwell.csv ../knn-32-blackwell.reference.csv log-scale_"
 
     "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-1024-hopper.csv ../knn-1024-hopper.reference.csv log-scale_"
     "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-1024-ampere.csv ../knn-1024-ampere.reference.csv log-scale_"
     "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-1024-volta.csv ../knn-1024-volta.reference.csv log-scale_"
+    "knn@single-graph-style@no-paper-print@auto-boundaries@with-log-scale ../knn-1024-blackwell.csv ../knn-1024-blackwell.reference.csv log-scale_"
 
     # Graphs for the paper
 
     "hist@two-graphs-in-row-style@first-graph@with-log-scale ../histogram-loremipsum-hopper.csv ../histogram-loremipsum-hopper.reference.csv paper_"
     "hist@two-graphs-in-row-style@second-graph@with-log-scale ../histogram-hexdump-hopper.csv ../histogram-hexdump-hopper.reference.csv paper_"
+
+    "hist@two-graphs-in-row-style@first-graph@with-log-scale ../histogram-loremipsum-blackwell.csv ../histogram-loremipsum-blackwell.reference.csv paper_"
+    "hist@two-graphs-in-row-style@second-graph@with-log-scale ../histogram-hexdump-blackwell.csv ../histogram-hexdump-blackwell.reference.csv paper_"
+
     "gol@single-graph-style@with-log-scale ../gol-hopper.csv ../gol-hopper.reference.csv paper_"
+    "gol@single-graph-style@with-log-scale ../gol-blackwell.csv ../gol-blackwell.reference.csv paper_"
+
     "knn@two-graphs-in-row-style@first-graph@with-log-scale ../knn-32-hopper.csv ../knn-32-hopper.reference.csv paper_"
-    "knn@two-graphs-in-row-style@second-graph@with-log-scale ../knn-32-ampere.csv ../knn-32-ampere.reference.csv paper_"
+    "knn@two-graphs-in-row-style@second-graph@with-log-scale ../knn-1024-hopper.csv ../knn-1024-hopper.reference.csv paper_"
+
+    "knn@two-graphs-in-row-style@first-graph@with-log-scale ../knn-32-blackwell.csv ../knn-32-blackwell.reference.csv paper_"
+    "knn@two-graphs-in-row-style@second-graph@with-log-scale ../knn-1024-blackwell.csv ../knn-1024-blackwell.reference.csv paper_"
+
 
     "hist@two-graphs-in-row-style@first-graph@with-log-scale@narrow-hist ../histogram-loremipsum-hopper.csv ../histogram-loremipsum-hopper.reference.csv paper_narrow_"
     "hist@two-graphs-in-row-style@second-graph@with-log-scale@narrow-hist ../histogram-hexdump-hopper.csv ../histogram-hexdump-hopper.reference.csv paper_narrow_"
+
+    "hist@two-graphs-in-row-style@first-graph@with-log-scale@narrow-hist ../histogram-loremipsum-blackwell.csv ../histogram-loremipsum-blackwell.reference.csv paper_narrow_"
+    "hist@two-graphs-in-row-style@second-graph@with-log-scale@narrow-hist ../histogram-hexdump-blackwell.csv ../histogram-hexdump-blackwell.reference.csv paper_narrow_"
+
     "gol@single-graph-style@with-log-scale@narrow-gol ../gol-hopper.csv ../gol-hopper.reference.csv paper_narrow_"
+    "gol@single-graph-style@with-log-scale@narrow-gol ../gol-blackwell.csv ../gol-blackwell.reference.csv paper_narrow_"
+
     "knn@two-graphs-in-row-style@first-graph@with-log-scale@narrow-knn ../knn-32-hopper.csv ../knn-32-hopper.reference.csv paper_narrow_"
-    "knn@two-graphs-in-row-style@second-graph@with-log-scale@narrow-knn ../knn-32-ampere.csv ../knn-32-ampere.reference.csv paper_narrow_"
+    "knn@two-graphs-in-row-style@second-graph@with-log-scale@narrow-knn ../knn-1024-hopper.csv ../knn-1024-hopper.reference.csv paper_narrow_"
+
+    "knn@two-graphs-in-row-style@first-graph@with-log-scale@narrow-knn ../knn-32-blackwell.csv ../knn-32-blackwell.reference.csv paper_narrow_"
+    "knn@two-graphs-in-row-style@second-graph@with-log-scale@narrow-knn ../knn-1024-blackwell.csv ../knn-1024-blackwell.reference.csv paper_narrow_"
 )
 
 echo "Printing graphs"
